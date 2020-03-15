c2e19f2f7523daa209bdb04f447640155689ea01
Aleksandr Maslov <aleksandr.m@riddec.com>
2020-03-15 23:30:10 +0200

    feat: move button to component

--- src/components/Company/Company.tsx
+++ src/components/Company/Company.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { ICompany } from '../../models/Company';
 import createCompanyInPipedrive from '../../api/create-company';
+import classnames from 'classnames';
 
-import styles from './Company.module.scss';
+import Button from '../Button/Button';
 
 const addButton = async name => {
     await createCompanyInPipedrive({ name });
@@ -15,20 +16,21 @@ const Company: React.FC<ICompany> = ({
     cvr,
 }) => {
     return (
-        <div className={styles.companyWrapper}>
-            <div className={styles.companyData}>
-                <h3>{name}</h3>
-                <p>
+        <div
+            className={classnames(
+                'd-flex border rounded',
+            )}
+        >
+            <div className="d-flex flex-column flex-fill pl-3 py-3 mw-100">
+                <h3 className="mb-0 text-truncate">{name}</h3>
+                <p className="mb-0">
                     {address}, {postal_code_and_city}
                 </p>
-                <p>CVR {cvr}</p>
+                <p className="mb-0">CVR {cvr}</p>
             </div>
-            <button
-                className={styles.addButton}
-                onClick={() => addButton(name)}
-            >
+            <Button appearance="success" onClick={() => addButton(name)}>
                 +
-            </button>
+            </Button>
         </div>
     );
 };
---
+++ src/models/Button.ts
@@ -0,0 +1,5 @@
+export interface IButton {
+    appearance?: string,
+    children: string,
+    onClick: any,
+}
