d0f8675518aa6b42bea56dd7c55e4d9bd7d0a542
Aleksandr Maslov <aleksandr.m@riddec.com>
2020-03-23 01:50:35 +0200

    feat: fix

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { IContainer } from '../../models/Container';
 import classnames from 'classnames';
 
+const style = {
+    height: '90%',
+} as React.CSSProperties;
+
 const Container: React.FC<IContainer> = ({ children, className }) => {
     return (
         <div
+        style={style}
             className={classnames(
-                'd-flex flex-column py-4 pl-4 h-100',
+                'd-flex flex-column py-4 pl-4',
                 className,
             )}
         >
