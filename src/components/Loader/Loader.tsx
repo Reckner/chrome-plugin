e547e563c0ff23e301d257137ba58d815e7892fb
Aleksandr Maslov <aleksandr.m@riddec.com>
2020-03-25 00:50:56 +0200

    feat: Loader style fix

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader: React.FC = () => {
     return (
-        <div className="d-flex justify-content-center align-items-center h-100">
+        <div className="d-flex justify-content-center align-items-center h-100 pr-4">
             <div className="spinner-border text-success" role="status">
                 <span className="sr-only">Loading...</span>
             </div>
