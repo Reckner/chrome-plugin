fea23227628bb61950f0fda42fc8580240d6398d
Aleksandr Maslov <aleksandr.m@riddec.com>
2020-03-29 00:34:31 +0200

    feat: add opacity

--- src/components/Confirmation/Confirmation.tsx
+++ src/components/Confirmation/Confirmation.tsx
@@ -10,6 +10,13 @@ const Confirmation: React.FC<IConfirmation> = ({
     setVisibility,
     target,
 }) => {
+    const styles = {
+        display: 'block',
+        height: 'auto',
+        top: '50%',
+        transform: 'translate(0%, -50%)',
+    } as React.CSSProperties;
+
     const handleYes = () => {
         if (target) {
             deleteCompany(target);
@@ -24,38 +31,41 @@ const Confirmation: React.FC<IConfirmation> = ({
     return (
         <>
             {isVisible ? (
-                <div
-                    style={{ top: '50%', display: 'block' }}
-                    id="removeConfirmation"
-                    className="modal fade show position-absolute"
-                >
-                    <div className="modal-dialog m-0 mx-4">
-                        <div className="modal-content border shadow">
-                            <div className="d-flex flex-column align-items-center p-3">
-                                <p className="mx-3">
-                                    Delete <strong>{children}</strong> company
-                                    from PipeDrive?
-                                </p>
-                                <div className="d-flex w-100">
-                                    <Button
-                                        appearance="danger"
-                                        className="mx-1 w-100"
-                                        onClick={handleYes}
-                                    >
-                                        Yes
-                                    </Button>
-                                    <Button
-                                        appearance="info"
-                                        className="mx-1 w-100"
-                                        onClick={handleNo}
-                                    >
-                                        No
-                                    </Button>
+                <>
+                    <div
+                        style={styles}
+                        id="removeConfirmation"
+                        className="modal fade show position-absolute"
+                    >
+                        <div className="modal-dialog m-0 mx-4">
+                            <div className="modal-content border shadow">
+                                <div className="d-flex flex-column align-items-center p-3">
+                                    <p className="mx-3">
+                                        Delete <strong>{children}</strong>{' '}
+                                        company from PipeDrive?
+                                    </p>
+                                    <div className="d-flex w-100">
+                                        <Button
+                                            appearance="danger"
+                                            className="mx-1 w-100"
+                                            onClick={handleYes}
+                                        >
+                                            Yes
+                                        </Button>
+                                        <Button
+                                            appearance="info"
+                                            className="mx-1 w-100"
+                                            onClick={handleNo}
+                                        >
+                                            No
+                                        </Button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                    <div className="modal-backdrop fade show position-absolute"></div>
+                </>
             ) : (
                 <></>
             )}
